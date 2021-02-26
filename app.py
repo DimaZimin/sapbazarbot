d7a722352d4afc4e40823f1fa8efd216e215be98
Dima Zimin <dmitriyzim@gmail.com>
2021-02-26 12:10:26 +0100

    minor fixes

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ if __name__ == '__main__':
     dp.loop.create_task(job_task(2500))
     dp.loop.create_task(blog_task(3000))
     dp.loop.create_task(blog_task_for_channel(3500))
-    dp.loop.create_task(check_new_answers_task(60))
+    dp.loop.create_task(check_new_answers_task(7000))
     executor.start_polling(dp, on_startup=on_startup)
